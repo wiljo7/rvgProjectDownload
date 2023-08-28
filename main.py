a214a0b07a229a50425f197b2ab21764c9c864dc
wiljo7 <wiljo7hdz@gmail.com>
2023-08-28 07:47:43 -0400

    update

--- main.py
+++ main.py
@@ -72,13 +72,13 @@ contract_status=[
         'EXPORTED TO NEW COMPANY']
 
 status_contract_user=st.multiselect('Status',contract_status)
-action=st.button('Search')
+#action=st.button('Search')
 
 
-if 'ALL' in status_contract_user:
-    status_contract_user.pop(0)
-else:
-    pass 
+#if 'ALL' in status_contract_user:
+#    status_contract_user.pop(0)
+#else:
+#    pass 
 
 
 def conexion_():
@@ -93,6 +93,90 @@ def conexion_():
     
     return cursor,connection
 
+
+
+
+
+
+def download_all_contracts_tables(cur,con,status_contract_user):
+     
+    contract_status_dict={
+        '1':'VACANT',
+        '2':'STARTED',
+        '3':'FINISHED',
+        '4':'CANCELLED',
+        '5':'READY BUT PENDING PAYABLE',
+        '6':'PROCESSING_PERMIT',
+        '7':'WAITING FOR THE CUSTOMER',
+        '8':'DOWNLOADING FILES',
+        '9':'SENT TO SALES OFFICE',
+        '10':'IN PRODUCTION QUEUE',
+        '11':'SENT TO ENGINEER',
+        '12':'WAITING FOR ADMINISTRATION',
+        '13':'EXPORTED TO NEW COMPANY'
+    }
+     
+    print('contract_status_dict',contract_status_dict)
+
+
+    #status_contract_user
+
+    cur.execute('select * from pre_contract')
+    data_preContract=[x for x in cur]
+    cur.execute("show columns from pre_contract")
+    columns1=[x[0] for x in cur]
+    
+    ###### iterar solo las columnas del precontract '
+    n=1
+    print('pre_contraxts////')
+    for x in columns1:
+        print(f'{n}',x)
+        n+=1
+    dfpreContract=pd.DataFrame(data=data_preContract,columns=columns1)
+
+
+    
+    
+    
+    cur.execute('select * from contract')
+    
+    data_Contract=[]
+    n=0
+    for x in cur:
+        x=list(x)
+        data_Contract.append(x)
+        #status
+        key_dict=x[31]
+        #### FILTER DATE FOR YEAR 
+        try:
+            y0=str(x[7]).split('-')
+            year_contract=y0[0]
+        except Exception as e: 
+            year_contract='NO DATE'
+
+        data_Contract.append((x[0],x[1],x[2],x[3],x[4],x[5],x[6],x[7],x[8],x[9],x[10],x[11],x[12],x[13],x[14],x[15],x[16],x[17],x[18],x[19],x[20],x[21],x[22],x[23],x[24],x[25],x[26],x[27],x[28],x[29],x[30],x[31],x[32],x[33],x[34],contract_status_dict[f'{key_dict}'],year_contract))
+        print(f'contract {n}',contract_status_dict[f'{key_dict}'])
+        n+=1
+
+    
+    cur.execute("show columns from contract")
+    columns2=[x[0] for x in cur]
+    n=1
+    columns2.append('Status')
+    columns2.append('initialYear')
+
+    print('contracts////')
+    for x in columns2:
+        print(f'{n}',x)
+        n+=1
+    dfContract=pd.DataFrame(data_Contract,columns=columns2)
+
+
+    #make_xls(dfpreContract,'Precontratos')
+    #make_xls(dfContract,'Contratos')
+
+
+    return dfpreContract,dfContract
 
 def contracts_status(cursor,conexion,list_status): 
     print(contracts_status)
@@ -136,9 +220,9 @@ def contracts_status(cursor,conexion,list_status):
     
     
     print('sentence',"select * from pre_contract where contractid is not NULL")
-    if year_check in [str(x) for x in range(2019,2024)]:
+    if year_check in [2019,2020,2021,2022,2023,2024,2025]:
 
-        cursor.execute(f"select * from pre_contract where contractid is not NULL and precontractDate like '{year_check}%'")
+        cursor.execute(f"select * from pre_contract where contractid is not NULL and precontractDate like '{year_check}-%'")
         data_proyectcontract=[x for x in cursor]
 
     else:
@@ -229,12 +313,14 @@ def contracts_status(cursor,conexion,list_status):
     data_contracts=[]
     for x in cursor:
         if x[0] in df1['contractId']:
-            print('project to contract')
+            pass
+		#print('project to contract')
         #print('clave',x[2])
         #print('valor',contract_status_dict[str(x[2])])
             data_contracts.append([x[0],x[1],contract_status_dict[str(x[2])],x[3],x[4],x[5],x[6],x[7],x[8],x[9],x[10],x[11],x[12]])
-        else: 
-            print('no project') 
+        else:
+	 
+            pass#print('no project') 
 
     df3=pd.DataFrame(data_contracts,columns=['contractId','contractNumber','contractStatus','contractDate','clientId','propertyNumber','startDate','scheduledFinishDate','actualFinishDate','deliveryDate','initialComment','intermediateComment','finalComment'])
     if name_result == '':
@@ -309,11 +395,79 @@ finally:
 
         #cursor.close()
         #connection.close()
-        print("MySQL connection is closed")
-        print('table_names',table_names)
         
-        if len(status_contract_user) > 0 and action == False: 
-            contracts_status(cursor,connection,status_contract_user)
-        else:
-            pass 
+        #download_all_contracts_tables(cursor,connection)
 
+        if 1==1:        
+            print("MySQL connection is closed")
+            print('table_names',table_names)
+            
+            if len(status_contract_user) > 0:# and action == False: 
+                #contracts_status(cursor,connection,status_contract_user)
+                rdfprecontract,rdfcontract=download_all_contracts_tables(cursor,connection,status_contract_user)
+
+
+                # filter status   
+
+                if 'ALL' in status_contract_user:
+                    st.dataframe(rdfcontract)
+
+                    if rdfcontract.empty:
+                        st.error('File not exists')
+                    else:
+                        
+                        make_xls(rdfcontract,f'./{name_result}')
+                        excel_file = f'./{name_result}.xlsx'
+
+            
+                        if os.path.exists(excel_file):
+                            with open(excel_file, 'rb') as f:
+                                data = f.read()
+                            st.download_button(
+                                label='Download Excel File',
+                                data=data,
+                                file_name=os.path.basename(excel_file),
+                                mime='application/vnd.ms-excel'
+                            )
+                                
+                        else:
+                            pass  
+                else:
+                    
+                    filter_rdfcontract1=rdfcontract[rdfcontract['Status'].isin(status_contract_user)]
+                    filter_rdfcontract2=filter_rdfcontract1[filter_rdfcontract1['initialYear']==year_check]   
+                    st.markdown('---')
+                    status_text='-'.join(status_contract_user)
+                    st.subheader(f'Table1 - All Contracts with status {status_text}')
+
+                    st.dataframe(filter_rdfcontract1)
+
+                    st.markdown('---')
+                    
+                    st.subheader(f'Table2 - All Contracts with status {status_text} and InitialDate {year_check}')
+                    
+                    st.dataframe(filter_rdfcontract2)
+
+                    if rdfcontract.empty:
+                        st.error('File not exists')
+                    else:
+                        
+                        make_xls(filter_rdfcontract2,f'./{name_result}')
+                        excel_file = f'./{name_result}.xlsx'
+
+            
+                        if os.path.exists(excel_file):
+                            with open(excel_file, 'rb') as f:
+                                data = f.read()
+                            st.download_button(
+                                label='Download Excel File Table2',
+                                data=data,
+                                file_name=os.path.basename(excel_file),
+                                mime='application/vnd.ms-excel'
+                            )
+                                
+                        else:
+                            pass
+
+        else:
+            print('bajando los excel')
